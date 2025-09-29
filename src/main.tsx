ac3c90cea4421d5a3e4b05b5266b1079843540ca
Gerwin <gerwin@Gerwins-iMac.fritz.box>
2025-09-29 13:15:15 +0200

    Initial viewer + Wochenplan 1 - 2029-09-29

--- src/main.tsx
+++ src/main.tsx
@@ -11,4 +11,17 @@ ReactDOM.createRoot(document.getElementById('root')!).render(
     </HashRouter>
   </React.StrictMode>
 );
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import { HashRouter } from 'react-router-dom';
+import App from './App.tsx';
+import './index.css';
+
+ReactDOM.createRoot(document.getElementById('root')!).render(
+  <React.StrictMode>
+    <HashRouter>
+      <App />
+    </HashRouter>
+  </React.StrictMode>
+);
 
